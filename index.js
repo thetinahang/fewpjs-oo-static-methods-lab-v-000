874b8fee24c989fe95356c474fb21656e65dbfad
Tina Hang <mstinahang@gmail.com>
2020-02-22 22:04:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Formatter {
   }
 
   static sanitize(string) {
-    return string.replace(/[^A-Za-z0-9-']+/g, '');
+    return string.replace(/[^a-zA-Z0-9-' ]+/g, '');
   }
 
   static titleize(string) {
